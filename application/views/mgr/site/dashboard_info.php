909c85761d5448f579465666cb037e9fae4e436d
ksg <ryuzuka1222>
2023-05-22 14:14:20 +0900

    메인 누적출고량 - 소수점 표기 - 관리자

--- application/views/mgr/site/dashboard_info.php
+++ application/views/mgr/site/dashboard_info.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <script type="text/javascript">
     $(document).ready(function () {
-        
+
         $('#btn-submit').click(function () {
             var obj;
 
@@ -13,28 +13,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 setFocus(obj);
                 return false;
             }
-            
+
             obj = $('#release_count');
             if (obj.val() == "") {
                 alert("누적 출고량을 입력해주세요.");
                 setFocus(obj);
                 return false;
             }
-            
+
             obj = $('#member_count');
             if (obj.val() == "") {
                 alert("배송 매니저를 입력해주세요.");
                 setFocus(obj);
                 return false;
             }
-            
+
             obj = $('#center_count');
             if (obj.val() == "") {
                 alert("운영 터미널센터를 입력해주세요.");
                 setFocus(obj);
                 return false;
             }
-            
+
             obj = $('#base_date');
             if (obj.val() == "") {
                 alert("기준일을 입력해주세요.");
@@ -49,24 +49,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 return false;
             }
         });
-        
+
         $(".onlynum").keypress(function (e) {
             if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
                 return false;
             }
         });
-        
+
         $('.calendar').datepicker({format: 'yyyy-mm-dd', todayHighlight: true});
-        
+
         $('.calendar').datepicker().on('changeDate', function (ev) {
             $('.datepicker').hide();
-        });        
+        });
     });
-    
+
     function onlyNumber() {
         if((event.keyCode<48)||(event.keyCode>57))
            event.returnValue=false;
-    }    
+    }
 </script>
 
 <div class="row">
@@ -86,28 +86,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                             <input type="text" id="client_count" name="client_count" maxlength="4" class="form-control input-sm onlynum" value="<?php if($mode == "update") echo $dashboard->client_count; ?>" onkeypress="javascript:onlyNumber();">
                         </div>
                     </div>
-                    
+
                     <div class="form-group">
                         <label class="col-sm-2 control-label"><span style="color:#FF0000;">*</span> 누적 출고량</label>
                         <div class="col-sm-2">
-                            <input type="text" id="release_count" name="release_count" maxlength="3" class="form-control input-sm onlynum" value="<?php if($mode == "update") echo $dashboard->release_count; ?>" onkeypress="javascript:onlyNumber();">
+                            <input type="text" id="release_count" name="release_count" maxlength="4" class="form-control input-sm" value="<?php if($mode == "update") echo $dashboard->release_count; ?>">
                         </div>
                     </div>
-                    
+
                     <div class="form-group">
                         <label class="col-sm-2 control-label"><span style="color:#FF0000;">*</span> 배송 매니저</label>
                         <div class="col-sm-2">
                             <input type="text" id="member_count" name="member_count" maxlength="4" class="form-control input-sm onlynum" value="<?php if($mode == "update") echo $dashboard->member_count; ?>" onkeypress="javascript:onlyNumber();">
                         </div>
                     </div>
-                    
+
                     <div class="form-group">
                         <label class="col-sm-2 control-label"><span style="color:#FF0000;">*</span> 운영 터미널센터</label>
                         <div class="col-sm-2">
                             <input type="text" id="center_count" name="center_count" maxlength="3" class="form-control input-sm onlynum" value="<?php if($mode == "update") echo $dashboard->center_count; ?>" onkeypress="javascript:onlyNumber();">
                         </div>
                     </div>
-                    
+
                     <div class="form-group">
                         <label class="col-sm-2 control-label"><span style="color:#FF0000;">*</span> 기준일</label>
                         <div class="col-sm-2">
@@ -120,13 +120,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         </div>
                     </div>
                 </div>
-                
+
                 <div class="box-footer text-center">
                     <a id="btn-submit" class="btn btn-primary btn-sm" style="width:80px;">저장</a>
                 </div>
             </form>
         </div>
-        
+
         <div class="box">
             <div class="box-header with-border">
                 <h3 class="box-title">수정이력</h3>
